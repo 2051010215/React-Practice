d01fee56170b60740576999697d9b3cb44de93a9
2051010215 <93323654+2051010215@users.noreply.github.com>
2024-02-18 21:59:57 +0700

    18.02

---
+++ src/views/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import Exmaple1 from '../components/exmaple1';
+
+function App() {
+  return (
+    <div className="App">
+      <Exmaple1/>
+    </div>
+  );
+}
+
+export default App;
